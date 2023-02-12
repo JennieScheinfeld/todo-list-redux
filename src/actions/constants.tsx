123d8598207e5b2a7b4164cee4beb9ccf08ef8b9
jennifer klein <jenniejoyness@gmail.com>
2023-02-12 23:33:24 +0200

    Remove uneeded files

--- src/actions/constants.tsx
+++
@@ -1,2 +0,0 @@
-export const ADD_TODO: string = "ADD_TODO"
-export const COMPLETED_TODO: string = "COMPLETED_TODO"
--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,3 @@ export type Todo = {
     text: string;
     completed: boolean;
 }
-
-export type Action = {
-    type: string;
-}
